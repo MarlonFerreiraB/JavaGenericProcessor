1ecc73805e4566d228594b272c52a39632c8069a
Marlon Ferreira <mferreirab03@gmail.com>
2025-06-24 14:43:23 -0300

    fix: corrigido nome da variavel tipoTransacao e do seu metodo get.

--- src/main/java/com/marlondev/processor/util/AgregadorDados.java
+++ src/main/java/com/marlondev/processor/util/AgregadorDados.java
@@ -1,13 +1,12 @@
 package main.java.com.marlondev.processor.util;
 
-import main.java.com.marlondev.processor.model.RegistroBase;
-import main.java.com.marlondev.processor.model.RegistroFInanceiro;
+import main.java.com.marlondev.processor.model.RegistroFinanceiro;
 
 import java.util.List;
 
 public class AgregadorDados {
 
-  public static <N extends Number, T extends RegistroFInanceiro> double somarValores(List<T> registros){
+  public static <N extends Number, T extends RegistroFinanceiro> double somarValores(List<T> registros){
       double somaTotal =0;
       for(T o: registros){
           somaTotal += o.getValue();
@@ -15,7 +14,7 @@ public class AgregadorDados {
       return somaTotal;
   }
 
-  public static <N extends Number, T extends RegistroFInanceiro> double CalcularMedia(List<T> registros){
+  public static <N extends Number, T extends RegistroFinanceiro> double CalcularMedia(List<T> registros){
       if (registros == null || registros.isEmpty()) {
           return 0.0; // Tratamento para lista vazia
       }
